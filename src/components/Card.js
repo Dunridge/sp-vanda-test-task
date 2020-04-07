671a531f72cb2e223c2df55bbb28813d856cfa45
Maksym Polinka <Polinka7max@gmail.com>
2020-04-07 17:13:31 +0300

    second try deploy

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Card = (props) => {
-    // see how to pass images as props in https://stackoverflow.com/questions/46417199/how-to-import-images-via-props-in-react-and-use-the-import-path
     console.log('Card', props);
 
     return (
