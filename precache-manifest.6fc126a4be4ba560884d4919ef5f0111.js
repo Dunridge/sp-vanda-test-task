3cc009b79b731f467b6758fe02116d02c553df18
Maksym Polinka <Polinka7max@gmail.com>
2020-04-07 17:08:43 +0300

    Updates

---
+++ precache-manifest.6fc126a4be4ba560884d4919ef5f0111.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16ebea0353c70b165c51f47902641c3",
+    "url": "/sp-vanda-test-task/index.html"
+  },
+  {
+    "revision": "d8135da0c6c0bd54c10a",
+    "url": "/sp-vanda-test-task/static/css/main.c3b2ec67.chunk.css"
+  },
+  {
+    "revision": "dd56b1963d978776c15e",
+    "url": "/sp-vanda-test-task/static/js/2.406f8e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sp-vanda-test-task/static/js/2.406f8e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8135da0c6c0bd54c10a",
+    "url": "/sp-vanda-test-task/static/js/main.18cd0b40.chunk.js"
+  },
+  {
+    "revision": "c11d9f3a1bc051d9f6bd",
+    "url": "/sp-vanda-test-task/static/js/runtime-main.07e75503.js"
+  },
+  {
+    "revision": "668a0b475230618c57f266258a583281",
+    "url": "/sp-vanda-test-task/static/media/card-task-profile.668a0b47.png"
+  },
+  {
+    "revision": "f231484a1bf9c8d7bbb9e81d5abb2b2b",
+    "url": "/sp-vanda-test-task/static/media/emblem-bigger-stick.f231484a.svg"
+  },
+  {
+    "revision": "6b335ebbc6f298a61adc6440963919c7",
+    "url": "/sp-vanda-test-task/static/media/emblem-smaller-stick.6b335ebb.svg"
+  },
+  {
+    "revision": "2676b2c3d64bf8de982ebf0e285def04",
+    "url": "/sp-vanda-test-task/static/media/empty-star.2676b2c3.svg"
+  },
+  {
+    "revision": "f3b2ea96c7ee3e191eda919e23beb89b",
+    "url": "/sp-vanda-test-task/static/media/filled-star.f3b2ea96.svg"
+  }
+]);
